0f255e83f1156390aeda46b98ce203155b6a861a
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-17 11:30:04 +0300

    First iteration with all tests green

--- src/main/kotlin/RnaTranscription.kt
+++ src/main/kotlin/RnaTranscription.kt
@@ -1,1 +1,8 @@
-fun transcribeToRna(dna: String): String = ""
+private val PROTEINS_MAP = mapOf(
+    'G' to 'C',
+    'C' to 'G',
+    'T' to 'A',
+    'A' to 'U'
+)
+
+fun transcribeToRna(dna: String): String = dna.map { c -> PROTEINS_MAP[c] }.joinToString("")
